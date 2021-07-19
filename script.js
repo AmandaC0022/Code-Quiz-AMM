ab0fe0a10cc6ab504f85495b63a41c956b06329f
Amanda Morgan <AmandaC0022@gmail.com>
2021-07-19 15:42:43 -0500

    game will now cycle through questions

--- script.js
+++ script.js
@@ -10,58 +10,47 @@ var endScreen = document.getElementById('end-screen');
 var initialsInput = document.getElementById('initials-input'); 
 var userInput = document.getElementsByClassName('user-input'); 
 var highscoreScreen = document.getElementById("highscore-screen"); 
-var highscoreBtn = document.getElementById("view-highscore-btn"); 
+var highscoreBtn = document.getElementById("view-highscores-btn"); 
 
 //Global Variables  
-var timerCount = 10; 
-var isWin = false;  
-var input; 
-var scores; 
+var timerCount = 60; 
+var isWin = false;
 
 timerId.textContent = "Score: " + timerCount;
 // Question Objects to cycle through, 6 questions total 
 var myQuestions = [
-    {
+    { //questionIndex = 0
         title:"What is the command we use to create a new file in the command line?", 
         choices:['touch' , 'cd' , 'mkdir' , 'pwd'], 
         answer:'touch', 
     },
-    {
+    { //questionIndex = 1
         title: "Which of the following is not a component of the box model?", 
         choices: ['The Content', 'The Display Property', 'The Border Property', 'The Padding Property'], 
         answer: 'The Display Property', 
     },
-    {
-        title:"Which of the following must be done in order to use `git init`?", 
-        choices: ["You must run 'git status' to check the status of your repository before running 'git init'." , "You must add and commit files before running 'git init'.", "You must have a repository created on GitHub before using 'git init'.", "You must be in you rproject folder when you run 'git init'."], 
-        answer: "You must be in you rproject folder when you run 'git init'.", 
-    },
-    {
+    { //questionIndex = 2
         title: "In JavaScript, what operator is used to assign a value to a declared variable?", 
         choices: ["Equal sign (=)", "Colon (:)", "Double-equal (==)", "Question Mark (?)"], 
         answer: "Equal sign (=)", 
     },
-    {
+    { //questionIndex = 3
         title: "How do we declare a conditional statement in JavaScript?", 
         choices: ["for loop", "while loop", "if ... else", "difference ... between"], 
         answer: "if ... else", 
     },
-    {
-        title: "From the given array which index is the letter 'b' on? ['a', 'b', 'c', 'd']", 
+    { //questionIndex = 4
+        title: "From the given array which index is the letter 'b' on?   ['a', 'b', 'c', 'd']", 
         choices: ['0', '1', '2', '3'], 
         answer: "1", 
     }
 ];
 
-//start game with question 1
-var questionIndex = 0;
-//links activeQuestion to question1 from question array
-var activeQuestion = myQuestions[questionIndex];
- 
 // This starts the game 
 startButton.onclick = startGame; 
 
 function startGame() {
+    //calls the countdown function 
     countdown(); 
     isWin = false; 
     //change screens - hide the start screen 
@@ -73,44 +62,58 @@ function startGame() {
     showQuestion();   
 } 
 
-function showQuestion() { 
-    //displays title from question1 
-    questionTitle.textContent = activeQuestion.title;
+var questionIndex = 0;
+//links activeQuestion to myQuestions Array 
+var activeQuestion = {
+    title: myQuestions[questionIndex].title, 
+    choices: myQuestions[questionIndex].choices, 
+    answer:myQuestions[questionIndex].answer,
+}
+
+function showQuestion() {  
+    //displays title of active Question 
+    questionTitle.textContent = activeQuestion.title;  
+
     //creates buttons for each choice option  
     activeQuestion.choices.forEach(function(choice) {
-        eachChoice = document.createElement("button"); 
+        var eachChoice = document.createElement("button"); 
         eachChoice.setAttribute("class", 'user-choice');  
-        eachChoice.setAttribute("value", choice); 
-        //this works, how can I pass this value into questionClick function!?!  
+        eachChoice.setAttribute("value", choice);  
         eachChoice.textContent = choice; 
-        eachChoice.addEventListener('click', questionClick);  
-        questionChoices.append(eachChoice);   
-    })
+        eachChoice.addEventListener('click', function(event){
+            userChoice = event.target.value;
+            questionClick(); 
+        });  
+        questionChoices.append(eachChoice);    
+    })  
 } 
 
 function questionClick() {
-    //not working...   
-    //var userChoice = 
-    console.log(activeQuestion.choice); 
-    console.log(activeQuestion.value); 
-    //if statement to check correct value / if correct go to the next question if not, substract time  
-    if (activeQuestion.choice === activeQuestion.answer) {
+    //if statement to check correct value / if correct go to the next question if not, substract time   
+    if (userChoice == activeQuestion.answer) {
+        //timer adds 20 seconds
         timerCount += 20; 
-        //moves to the next question 
-        questionIndex++;   
+        //adds +1 to question Index  
+        questionIndex++; 
+        if (questionIndex === myQuestions.length) {
+            isWin = true;
+            timerId.textContent = "Score: " + timerCount; 
+            quizOver(); 
+        }
         console.log(questionIndex); 
-        showQuestion; 
+        //updates the active question 
+        activeQuestion = myQuestions[questionIndex]; 
+        //So that the choices update but do not show up as text 
+        questionChoices.textContent = " ";
+        //calls the showQuestion function 
+        showQuestion(); 
     } else {
         timerCount -= 5;  
         var whoopsText = document.createElement('p'); 
         whoopsText.textContent = "Whoops! Try Again!"; 
         whoopsText.setAttribute('style', 'color:red; font-size:1em;'); 
         questionTitle.append(whoopsText); 
-    }
-    if (questionIndex === myQuestions.length) {
-        isWin = true; 
-        quizOver; 
-    }
+    } 
 }
 
 function quizOver() {
@@ -179,26 +182,23 @@ function viewHighScore() {
     highscoreScreen.append(topScoresInfo); 
 }
 
-//highscoreBtn.onclick = viewHighScore; 
-
-var highScoreObject = {
-    initials:[], 
-    userScore:[], 
-}
+highscoreBtn.onclick = viewHighScore; 
 
 function highScore() { 
     //save both initials and score into an object then push them into the local storage 
-    userInitials = input.value; 
-    highScoreObject.initials.push(userInitials); 
-    highScoreObject.userScore.push(timerCount); 
-    //highScoreObject.userScore = localStorage.getItem('score'); 
-    for (i = 0; i < 5; i++) {
-        initialsInput.textContent = highScoreObject.initials + ": " + highScoreObject.userScore;
-        localStorage.setItem("initials", userInitials); 
-        localStorage.setItem("score", timerCount);  
-    }
-    var btn = document.createElement('button'); 
-    btn.textContent = "Ok"; 
-    initialsInput.append(btn); 
-    btn.onclick = viewHighScore; 
+    
+
+
+    // userInitials = input.value; 
+    // highScoreObject.initials.push(userInitials); 
+    // highScoreObject.userScore.push(timerCount); 
+    // //highScoreObject.userScore = localStorage.getItem('score'); 
+    // initialsInput.textContent = highScoreObject.initials + ": " + highScoreObject.userScore;
+    // localStorage.setItem("initials", userInitials); 
+    // localStorage.setItem("score", timerCount);  
+    
+    // var btn = document.createElement('button'); 
+    // btn.textContent = "Ok"; 
+    // initialsInput.append(btn); 
+    // btn.onclick = viewHighScore; 
 }; 
